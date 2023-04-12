a3f5959e764b39b5e08b93a320399eb7866a2e9d
violentiy1997 <violentiy1997@mail.ru>
2023-04-12 12:15:07 +0300

    add pagination in users.jsx

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,15 +5,39 @@ import userPhoto from '../../assets/images/avatar.png'
 
 
 class Users extends React.Component {
-   componentDidMount() {
-      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {     //так при загрузке получаем пользователей
+   componentDidMount() {      //этот метод встроен в React.Component и говорит компоненте что она была отрисована в HTML
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {     //так при загрузке получаем пользователей
+      this.props.setUsers(response.data.items)
+      this.props.setTotalUsersCount(response.data.totalCount)
+      })
+   }
+
+   onPageChanged = (pageNumber) => {
+      this.props.setCurrentPage(pageNumber)
+      axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {     //так при загрузке получаем пользователей
       this.props.setUsers(response.data.items)
       })
    }
 
-   
    render() {
+
+      let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
+      let pages = []
+      for (let i=1; i <= pagesCount; i++) {
+         pages.push(i)
+      }
+      //карусель количества переключения страниц
+      let curP = this.props.currentPage;
+      let curPF = ((curP - 5) < 0) ?  0  : curP - 5 ;
+      let curPL = curP + 5;
+      let slicedPages = pages.slice( curPF, curPL);
+
       return <div>  
+      <div>
+         {slicedPages.map(pages => {
+            return <span className={this.props.currentPage === pages && styles.selectedPage} onClick={(e) => { this.onPageChanged(pages) }}>{pages}</span>
+         })}
+      </div>
       {
          this.props.users.map(users => <div key={users.id}>
             <span>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,10 +1,15 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET-USERS'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
 
 
 let initialState = {
-      users: []
+      users: [],
+      pageSize: 5,
+      totalUsersCount: 0,
+      currentPage: 2
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -25,7 +30,13 @@ const usersReducer = (state = initialState, action) => {
             return users
          } )}  
       case SET_USERS: {
-         return {...state, users: [ ...state.users, ...action.users ]}       //берем из action users и добавляем в state
+         return {...state, users: action.users}       //берем из action users и добавляем в state
+      }
+      case SET_CURRENT_PAGE: {
+         return {...state, currentPage: action.currentPage}
+      }
+      case SET_TOTAL_USERS_COUNT: {
+         return {...state, totalUsersCount: action.count}
       }
       default: 
          return state
@@ -51,6 +62,19 @@ export const setUsersActionCreator = (users) => {
    return {
       type: SET_USERS,
       users
+   }
+}
+
+export const setCurrentPageActionCreator = (currentPage) => { 
+   return {
+      type: SET_CURRENT_PAGE,
+      currentPage
+   }
+}
+export const setUsersTotalCountActionCreator = (totalUsersCount) => { 
+   return {
+      type: SET_TOTAL_USERS_COUNT,
+      count: totalUsersCount
    }
 }
 
