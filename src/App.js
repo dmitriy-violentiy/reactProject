0ab82991c13116def4e17a4e248ee5e83bd36e30
violentiy1997 <violentiy1997@mail.ru>
2023-04-18 16:19:50 +0300

    add login in project

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import "./App.css";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import FriendsContainer from "./components/Friends/FriendsContainer";
-import Header from "./components/Header/Header";
 import Music from "./components/Music/Music";
 import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 
 const App = (props) => {
    return (
       <BrowserRouter>
          <div className="app-wrapper">
-         <Header />
+         <HeaderContainer />
          <Navbar />
          <div className="app-wrapper-content">
             <Routes>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import classes from './Header.module.css'
+import { NavLink } from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
    return (
       <header className={classes.header}>
          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Twitter-logo.svg/934px-Twitter-logo.svg.png"/>
-         </header>
+
+         <div className={classes.loginBlock}>
+            { props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink> }
+         </div>
+      </header>
    )
 }
 
