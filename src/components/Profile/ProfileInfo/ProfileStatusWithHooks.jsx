5f3818467a0e572cf8e01aec1fc130382c676da2
violentiy1997 <violentiy1997@mail.ru>
2023-05-02 14:45:40 +0300

    add useEffect in project

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import classes from './ProfileInfo.module.css'
 
 const ProfileStatusWithHooks = (props) => {
 
    let [editMode, setEditMode] = useState(false)      //присвоили в editMode - false, а в setEditMode функцию, меняющую это значение
    let [status, setStatus] = useState(props.status)
+
+   useEffect( () => {
+      setStatus(props.status)
+   }, [props.status] )     //указали, что зависем от props.status и будем перерисовывать всегда, когда будет новый props.status отличаться от старого
    
    const activateEditMode = () => {
       setEditMode(true)
