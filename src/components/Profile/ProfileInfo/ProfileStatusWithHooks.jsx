d5f93268f589d490cfdc24902cba4ea136b840c0
violentiy1997 <violentiy1997@mail.ru>
2023-05-02 13:47:16 +0300

    add hook in project

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import classes from './ProfileInfo.module.css'
+
+const ProfileStatusWithHooks = (props) => {
+
+   let [editMode, setEditMode] = useState(false)      //присвоили в editMode - false, а в setEditMode функцию, меняющую это значение
+   let [status, setStatus] = useState(props.status)
+   
+   const activateEditMode = () => {
+      setEditMode(true)
+   }
+
+   const deactivateEditMode = () => {
+      setEditMode(false)
+      props.updateStatus(status)     //обновляем статус, взяв его из value input-a
+   } 
+
+   const onStatusChange = (event) => {
+      setStatus(event.currentTarget.value)
+   }
+
+   return (
+      //условие вывода span или input
+      <div>
+         {  !editMode &&
+            <div>
+               <span onDoubleClick={activateEditMode}><b>Статус: </b>{props.status || "-----------"}</span>
+            </div>
+         }
+         { editMode &&
+            <div>
+               <input onChange={ onStatusChange } autoFocus={true} onBlur={ deactivateEditMode } value={ status } />
+            </div>
+         }
+      </div>
+   )
+
+}
+
+export default ProfileStatusWithHooks;
