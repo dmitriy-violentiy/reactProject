cd047dff6848587c3d1c4e78a2f11585b7818d38
violentiy1997 <violentiy1997@mail.ru>
2023-05-03 11:53:42 +0300

    add React.memo in MyPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,6 +5,7 @@ import { Field, reduxForm } from "redux-form";
 import { maxLengthCreator, required } from "../../../utils/validators/validators";
 import { Textarea } from "../../common/FormsControls/FormsControls";
 
+
 let AddNewPostForm = (props) => {
    //когда данные собраны, вызываем функцию handleSubmit, которая перерисовывает каждое действие
    return(
@@ -20,33 +21,40 @@ let AddNewPostForm = (props) => {
 }
 
 let AddNewPostFormRedux = reduxForm({form: "ProfileAddNewPostForm"})(AddNewPostForm)      //обернули AddNewPostForm в reduxForm, дали уникальное имя
+const MyPosts = React.memo((props) => {
 
-const MyPosts = (props) => {
+   //использовалось в классовых компонентах
+   /* shouldComponentUpdate(nextProps, nextState) {
+      return nextProps != this.props || nextState != this.state 
+   } */
+   
+      console.log('xxx');
 
-   //создали новый массив объектов на основе массива posts, который выведет столько постов, сколько придет 
-   let postsElements = props.posts.map( (post) => {
-      return(
-         <Post message={post.messege} like={post.likesCount} key={post.id}/>
-      )
-   } )
+      //создали новый массив объектов на основе массива posts, который выведет столько постов, сколько придет 
+      let postsElements = props.posts.map( (post) => {
+         return(
+            <Post message={post.messege} like={post.likesCount} key={post.id}/>
+         )
+      } )
 
-   //функция, которая считывает, что ввел пользователь
-   let onAddPost = (values) => {
-      props.addPost(values.newPostText)
-   }
+      //функция, которая считывает, что ввел пользователь
+      let onAddPost = (values) => {
+         props.addPost(values.newPostText)
+      }
 
-   return (
-      <div className={classes.postsBlock}>
-         <h3>My posts</h3>
-         <AddNewPostFormRedux onSubmit={onAddPost}/>
-         <div className={classes.posts}>
-            {
-               postsElements
-            }
+      return (
+         <div className={classes.postsBlock}>
+            <h3>My posts</h3>
+            <AddNewPostFormRedux onSubmit={onAddPost}/>
+            <div className={classes.posts}>
+               {
+                  postsElements
+               }
+            </div>
          </div>
-      </div>
-   )
-}
+      )
+   
+})
 
 
 
