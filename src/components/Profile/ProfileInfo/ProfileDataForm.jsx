7248aa3e36f1affb0c7fdfaf6a8718725a593602
violentiy1997 <violentiy1997@mail.ru>
2023-05-16 17:21:22 +0300

    fix bug changed profile info

--- src/components/Profile/ProfileInfo/ProfileDataForm.jsx
+++ src/components/Profile/ProfileInfo/ProfileDataForm.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+/* import React from "react"
 import { Input, Textarea } from "../../common/FormsControls/FormsControls"
 import { Field, reduxForm } from "redux-form"
 import classes from './ProfileDataForm.module.css'
@@ -25,4 +25,43 @@ const ProfileDataForm = ({handleSubmit}) => {
 
 const ProfileDataFormReduxForm = reduxForm({ form: 'edit-profile'})(ProfileDataForm)
 
-export default ProfileDataFormReduxForm
+export default ProfileDataFormReduxForm */
+
+import React from "react"
+import classes from './ProfileDataForm.module.css'
+import { useForm } from "react-hook-form"
+
+const ProfileDataForm = ({profile, ...props}) => {
+   const {
+      register,
+      formState: {
+         errors,
+         isValid
+      },
+      handleSubmit,
+   } = useForm({
+      mode: "onBlur",
+   })
+
+   return (
+      <form onSubmit={handleSubmit(props.onSubmit)} className={classes.profileDataForm}>
+         <div>
+            <b>Name:</b> 
+               <label title={errors?.fullName}>
+                  <input className={classes.profileDataForm__nameInput} placeholder="Full name" defaultValue={profile.fullName} {...register('fullName', {required: true})} required />
+               </label>
+         </div>
+         <div className={classes.profileDataForm__jobInput_wrap}>
+            <b>A job status:</b> <input className={classes.profileDataForm__jobInput} placeholder="" defaultValue={profile.lookingForAJob} type="checkbox" {...register('lookingForAJob', {})} />
+         </div>
+         <div>
+            <b>My professional skills:</b> <textarea className={classes.profileDataForm__skillsInput} placeholder="My professional skills" defaultValue={profile.lookingForAJobDescription} {...register('lookingForAJobDescription', {required: true})} required />
+         </div>
+         <div>
+            <b>About me:</b> <textarea className={classes.profileDataForm__aboutInput} placeholder="About me" defaultValue={profile.aboutMe} {...register('aboutMe', {required: true})} required />
+         </div>
+         <input className={classes.profileDataForm__button} type="submit"/>
+      </form>
+   )
+}
+export default ProfileDataForm
